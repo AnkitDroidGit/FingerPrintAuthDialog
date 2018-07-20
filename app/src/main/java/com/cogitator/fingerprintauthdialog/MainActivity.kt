c81cb9069f89983e03c7e25c9b6cfabe7413d917
Ankit Kumar <ankitdroiddeveloper@gmail.com>
2018-07-20 13:54:08 +0800

    started code

--- app/src/main/java/com/cogitator/fingerprintauthdialog/MainActivity.kt
+++ app/src/main/java/com/cogitator/fingerprintauthdialog/MainActivity.kt
@@ -2,11 +2,33 @@ package com.cogitator.fingerprintauthdialog
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.hardware.fingerprint.FingerprintManagerCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            val manager = FingerprintManagerCompat.from(this)
+
+            if (manager.isHardwareDetected && manager.hasEnrolledFingerprints()) {
+                showFingerprintAuth()
+            } else {
+                Snackbar.make(view, "Fingerprint authentication is not supported.", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun showFingerprintAuth() {
+        val dialog = FingerprintDialog.newInstance(
+                "Sign In",
+                "Confirm fingerprint to continue."
+        )
+        dialog.show(supportFragmentManager, FingerprintDialog.FRAGMENT_TAG)
     }
 }
